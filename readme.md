65b48c46f1e1f53ae01ad24400d207e49b7bfff1
Peter Verhas <peter@verhas.com>
2019-04-27 10:35:01 +0200

    documentation

--- readme.md
+++ readme.md
@@ -1,2 +1,80 @@
 # Exceptional
 
+>Get rid of exceptions you'd ignore anyway
+
+Exceptional is a single class (and some inner classes) that implements
+methods that make it look very much like an `java.util.Optional`. But it
+is not `Optional`.
+
+Exceptional is a value, which is provided by a supplier (but not a
+`java.util.function.Supplier`) which may throw an exception. So when you
+write
+
+```java
+Exceptional.of(() -> throw new Exception()).orElse("Alma")
+```
+
+what you get is `"Alma"`. The major difference from `Optional` is that
+the argument of the method `of()` is a supplier (to be specific an
+`javax0.Exceptional.ThrowingSupplier`). In case the supplier throws an
+`Exception` then the value inside the returned `Exceptional` object will
+be "empty".
+
+On an `Exceptional` object you can call `or()`, `map()`, `flatMap()` and
+so on. Essentially the same (almost) methods that are defined in the
+class `java.util.Optional`. There are some differences though.
+
+The methods that return `Optional` in the `Optional` class here return
+`Exceptional`. If it is a surprise you probably should not read on.
+
+The static methods `of()` and `ofNullable()` and the non-static methods
+`or()` and `orNullable()` accept `javax0.Exceptional.ThrowingSupplier`
+as argument. (Also note that `orNullable()` barely exists in
+`Optional` if at all.)
+
+The methods `map()` and `flatMap()` accept
+`javax0.Exceptional.ThrowingFunction` as argument. If any of these throw
+exception the resulting `Exceptional` will be empty.
+
+These are the non-taxative features raw and uncooked. What is it good
+for?
+
+## Get rid of exceptions
+
+You have an old code
+
+```java
+// this is a totally made up code, we have never seen anything close
+  String clientId = null;
+  try {
+    clientId = req.getAttribute("klientele");
+  }catch(ParseException pe){
+  }
+  if( clientId == null ){
+    clientId = getDebugClient();
+  }
+```
+
+Using `Exceptional` you can
+
+```java
+  final String clientId = Exceptional.<String>ofNullable(() -> req.getAttribute("klientele"))
+                                     .orNullable(() -> getDebugClient())
+                                     .orElse(null);
+```
+
+it is a matter of taste which one you prefer.
+
+You can also replace the good old
+
+```java
+  Integer value = part.dev.item(0). ...getter():
+```
+
+prone to `NullPointerException` to
+
+```java
+  Integer value = Exceptional.ofNullable(() -> part.dev.item(0). ...getter()).orElse(null);
+```
+
+if that is more readable.
--- src/test/java/javax0/TestExceptional.java
+++ src/test/java/javax0/TestExceptional.java
@@ -6,64 +6,62 @@ import org.junit.jupiter.api.Test;
 
 class TestExceptional {
 
-    private static Object thrower() throws Exception {
-        throw new Exception();
-    }
-
-    private static Object object() {
-        return "korte";
-    }
-
-    private static Object nuller() {
-        return null;
-    }
-
     @Test
     @DisplayName("when 'of' throws exception then orElse is taken into account")
     void testThrowOrElse() {
-        Assertions.assertEquals("Alma", Exceptional.of(TestExceptional::thrower).orElse("Alma"));
+        Assertions.assertEquals("Alma", Exceptional.of(() -> {
+            throw new Exception();
+        }).orElse("Alma"));
     }
 
     @Test
     @DisplayName("when 'of' null then it throws NPE")
     void testNullOfThows() {
-        Assertions.assertThrows(NullPointerException.class, () -> Exceptional.of(TestExceptional::nuller).orElse("Alma"));
+        Assertions.assertThrows(NullPointerException.class, () -> Exceptional.of(() -> null).orElse("Alma"));
     }
 
     @Test
     @DisplayName("when 'of' null then orElse is taken into account")
     void testNullOrElse() {
-        Assertions.assertEquals("Alma", Exceptional.ofNullable(TestExceptional::nuller).orElse("Alma"));
+        Assertions.assertEquals("Alma", Exceptional.ofNullable(() -> null).orElse("Alma"));
     }
 
     @Test
     @DisplayName("when 'ofNullable' is null and 'or' throws then orElse is taken into account")
     void testThrowsOfNullOrElse() {
-        Assertions.assertEquals("Alma", Exceptional.ofNullable(TestExceptional::nuller).or(TestExceptional::thrower).orElse("Alma"));
+        Assertions.assertEquals("Alma", Exceptional.ofNullable(() -> null).or(() -> {
+            throw new Exception();
+        }).orElse("Alma"));
     }
 
     @Test
     @DisplayName("when something has value in the line orElse is ignored")
     void testValueOrElseIgnored() {
-        Assertions.assertEquals("korte", Exceptional.ofNullable(TestExceptional::nuller).or(TestExceptional::thrower).or(TestExceptional::object).orElse("Alma"));
+        Assertions.assertEquals("korte", Exceptional.ofNullable(() -> null).or(() -> {
+            throw new Exception();
+        }).or(() -> "korte").orElse("Alma"));
     }
 
     @Test
     @DisplayName("when something has value in the line and there is orNullable orElse is ignored")
     void testNullOrNullableElse() {
-        Assertions.assertEquals("korte", Exceptional.of(TestExceptional::thrower).orNullable(TestExceptional::nuller).or(TestExceptional::object).orElse("Alma"));
+        Assertions.assertEquals("korte", Exceptional.of(() -> {
+            throw new Exception();
+        }).orNullable(() -> null).or(() -> "korte").orElse("Alma"));
     }
 
     @Test
     @DisplayName("after a value 'or( () -> null)' does not matter")
     void testNullOrNullIsIgnored() {
-        Assertions.assertEquals("korte", Exceptional.<Object>of(() -> "korte").or(TestExceptional::nuller).or(TestExceptional::object).orElse("Alma"));
+        Assertions.assertEquals("korte", Exceptional.<Object>of(() -> "korte").or(() -> null).or(() -> "korte").orElse("Alma"));
     }
 
     @Test
     @DisplayName("when map throws exception it is empty again")
     void testLameMap() {
-        Assertions.assertEquals("Alma", Exceptional.<Object>of(() -> "korte").map(s -> thrower()).orElse("Alma"));
+        Assertions.assertEquals("Alma", Exceptional.<Object>of(() -> "korte").map(s -> {
+            throw new Exception();
+        }).orElse("Alma"));
     }
 
     @Test
@@ -75,7 +73,7 @@ class TestExceptional {
     @Test
     @DisplayName("when flatMap throws exception it is empty again")
     void testFlatMap() {
-        Assertions.assertEquals("Alma", Exceptional.<Object>of(() -> "korte").flatMap(s -> Exceptional.of(TestExceptional::nuller)).orElse("Alma"));
+        Assertions.assertEquals("Alma", Exceptional.<Object>of(() -> "korte").flatMap(s -> Exceptional.of(() -> null)).orElse("Alma"));
     }
 
     @Test
